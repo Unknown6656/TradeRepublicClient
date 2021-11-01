c3a62b135e0905e5c46829c27393b99abf342dd7
unknown6656 <unknown84@gmx.de>
2021-11-01 10:54:15 +0100

    switched to async, added fetcher for personal data, made login/logout methods more robust, and added a keepalive function

--- API/APIConnection.cs
+++ API/APIConnection.cs
@@ -1,14 +1,18 @@
-﻿using System.IO.Pipes;
+﻿using System.Text.RegularExpressions;
+using System.Diagnostics;
 using System.Text;
-using System.Text.RegularExpressions;
 
-using OpenQA.Selenium;
-using OpenQA.Selenium.DevTools.V85.DeviceOrientation;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
+using Microsoft.Win32;
+
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System;
 
 namespace TradeRepublic;
+
+
+// TODO : remove autopinned msedge shortcut in "C:\Users\unknown6656\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
 
 
 /// <summary>
@@ -19,6 +23,9 @@ public static class API
 {
     public static string BaseURL { get; set; } = "https://app.traderepublic.com";
     public static string TemporaryDirectory { get; set; } = "./temporary-files";
+
+    private const string HKEY_MSEDGE = @"Software\Microsoft\Edge";
+    private const string HKEY_AUTOPIN = "TaskBarAutoPin";
 
     private static readonly object _mutex = new();
     private static APIConnection? _connector = null;
@@ -40,6 +47,13 @@ public static class API
                 if (!temp_dir.Exists)
                     temp_dir.Create();
 
+                using (RegistryKey? hkey = Registry.CurrentUser.OpenSubKey(HKEY_MSEDGE, true))
+                    if (hkey is { })
+                    {
+                        hkey.SetValue(HKEY_AUTOPIN, 0, RegistryValueKind.DWord);
+                        hkey.Flush();
+                    }
+
                 FileInfo msedge = Locate("msedge.exe") ?? new("C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe");
                 FileInfo msedgedriver = new(Path.Combine(temp_dir.FullName, "msedgedriver.exe"));
 
@@ -53,8 +67,11 @@ public static class API
                     BinaryLocation = msedge.FullName,
                     LeaveBrowserRunning = false,
                 };
+                options.AddArgument("--log-level=3");
                 options.AddArgument("--disable-infobars");
-                options.AddAdditionalCapability("useAutomationExtension", false);
+                options.AddArgument("--window-size=1920x1080");
+                options.AddArgument("--auto-open-devtools-for-tabs");
+                options.AddAdditionalEdgeOption("useAutomationExtension", false);
                 options.AddExcludedArgument("enable-automation");
 
                 EdgeDriver driver = new EdgeDriver(temp_dir.FullName, options);
@@ -101,9 +118,18 @@ public static class API
 public sealed class APIConnection
     : IDisposable
 {
+    private const int KEEP_ALIVE_TIMEOUT_MS = 180_000;
+    private readonly Stopwatch _sw_last_action = new();
+    private readonly object _keep_alive_mutex = new();
+    private Task? _keep_alive_task = null;
+
+    private ushort? _current_pin = null;
+    private string? _current_phone = null;
+
     private EdgeDriver _driver;
     private DirectoryInfo _temp_dir;
     private string _baseURL;
+
 
 
     /// <summary>
@@ -142,6 +168,9 @@ public sealed class APIConnection
 
     private const string URI__LOGIN = "/login#layout__main";
     private const string URI__PORTFOLIO = "/portfolio?timeframe=1y";
+    private const string URI__SETTINGS_PERSONAL = "/settings/personal";
+    private const string URI__SETTINGS_ACCOUNTS = "/settings/accounts";
+    private const string URI__SETTINGS_TAX = "/settings/tax";
     private static readonly Regex REGEX__PHONE_NUMBER = new($@"(\+|00)(?<country>{string.Join('|', accepted_country_codes)})0?(?<number>\d+)", RegexOptions.Compiled);
     private static readonly By SEL__COOKIE_ACCPET_BUTTON = By.CssSelector(".app__dataConsent button.consentCard__action");
     private static readonly By SEL__PHONE_NUMBER_INPUT = By.Id("loginPhoneNumber__input");
@@ -150,10 +179,11 @@ public sealed class APIConnection
     private static readonly By SEL__PIN_LOGIN_ERROR = By.ClassName("loginPin__errorMessage");
     private static readonly By SEL__SMS_LOGIN_ERROR = By.ClassName("smsCode__statusMessage");
     private static readonly By SEL__LOGOUT_BUTTON = By.ClassName("settings__sessionControl");
-
+    private static readonly By SEL__SETTINGS_PANEL_PERSONAL = By.CssSelector("#settingsTabPanels__PersonalTabPanel dd");
+    private static readonly By SEL__SETTINGS_PANEL_ACCOUNTS = By.CssSelector("#settingsTabPanels__AccountsTabPanel dd");
+    private static readonly By SEL__SETTINGS_PANEL_TAX = By.CssSelector("#settingsTabPanels__TaxTabPanel dd");
     private const string SEL__COUNTRY_CODE_OPTION = "[id|=\"countryCode\"]";
     private const string SEL__COUNTRY_CODE_BUTTON = ".phoneNumberInput__countryCode button span";
-
     private const string ATTR__COUNTRY_CODE_OPTION_SELECTED = "aria-selected";
 
     /// <summary>
@@ -165,7 +195,7 @@ public sealed class APIConnection
     /// Indicates whether the user is currently logged in.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown if the API connection has already been closed or disposed.</exception>
-    public bool IsLoggedIn => IsDisposed ? throw new ObjectDisposedException(nameof(APIConnection)) : WaitForElements(SEL__LOGOUT_BUTTON, 1_000) is { Length: > 0 };
+    public bool IsLoggedIn => WaitForElements(SEL__LOGOUT_BUTTON).GetAwaiter().GetResult() is { Length: > 0 };
 
 
     internal APIConnection(EdgeDriver driver, DirectoryInfo temp_dir, string baseURL)
@@ -174,8 +204,6 @@ public sealed class APIConnection
         _baseURL = baseURL;
         _driver = driver;
     }
-
-    private void InjectJQuery() => _driver.ExecuteScript(Resources.jquery); 
 
     /// <summary>
     /// Tries to log in using the given credentials. An <see cref="ArgumentException"/> will be thrown if the credentials are incorrect.
@@ -186,26 +214,38 @@ public sealed class APIConnection
     /// <returns>Returns whether the login process has been successful.</returns>
     /// <!-- <exception cref="ArgumentException">Thrown if the credentials are invalid.</exception> -->
     /// <exception cref="ObjectDisposedException">Thrown if the API connection has already been closed or disposed.</exception>
-    public LoginStatus Login(string phone, ushort pin, Func<ushort> sms_callback)
+    public async Task<LoginStatus> Login(string phone, ushort pin, Func<ushort> sms_callback)
     {
         if (IsDisposed)
             throw new ObjectDisposedException(nameof(APIConnection));
+        else if (IsLoggedIn)
+            if (_current_phone is { } &&
+                _current_phone.Replace("+", "00").Where(char.IsDigit).SequenceEqual(
+                         phone.Replace("+", "00").Where(char.IsDigit)))
+                return LoginStatus.Success;
+            else
+                Logout();
 
         phone = new string(phone.ToLower().Where(c => char.IsDigit(c) || c is '+').ToArray());
 
+        if (phone.StartsWith("00"))
+            phone = '+' + phone[2..];
+
+        _current_phone = phone;
+        _current_pin = Math.Min(pin, (ushort)9999);
+
         Match match = REGEX__PHONE_NUMBER.Match(phone);
-        int country = 0;
 
         if (!match.Success)
             return LoginStatus.Failure_InvalidPhoneNumber;
-        if (!int.TryParse(match.Groups["country"].Value, out country))
+        if (!int.TryParse(match.Groups["country"].Value, out int country))
             return LoginStatus.Failure_InvalidPhoneNumber;
         if (!accepted_country_codes.Contains(country))
             return LoginStatus.Failure_InvalidPhoneNumber;
 
         phone = match.Groups["number"].Value;
 
-        string code = Math.Min(pin, (ushort)9999).ToString("D4");
+        string code = _current_pin.Value.ToString("D4");
 
         _driver.Url = _baseURL + URI__LOGIN;
         _driver.Navigate();
@@ -240,8 +280,31 @@ public sealed class APIConnection
         if (_driver.FindElement(SEL__SMS_LOGIN_ERROR).Text is string sms_error && !string.IsNullOrWhiteSpace(sms_error))
             return LoginStatus.Failure_WrongSMSCode;
 
-        if (WaitForElements(SEL__LOGOUT_BUTTON, 30_000) is { Length: > 0 })
+        await Task.Delay(2000);
+
+        _driver.Url = _baseURL + URI__PORTFOLIO;
+        _driver.Navigate();
+
+        if (IsLoggedIn)
+        {
+            _sw_last_action.Restart();
+
+            lock (_keep_alive_mutex)
+                _keep_alive_task = Task.Factory.StartNew(async delegate
+                {
+                    while (IsLoggedIn)
+                        if (_sw_last_action.ElapsedMilliseconds > KEEP_ALIVE_TIMEOUT_MS)
+                            lock (_keep_alive_mutex)
+                            {
+                                _driver.Url = _baseURL + URI__PORTFOLIO;
+                                _driver.Navigate();
+                            }
+                        else
+                            await Task.Delay(1000);
+                });
+
             return LoginStatus.Success;
+        }
         else
             return LoginStatus.Failure_LoginTimeout;
     }
@@ -255,6 +318,9 @@ public sealed class APIConnection
     /// <exception cref="ObjectDisposedException">Thrown if the API connection has already been closed or disposed.</exception>
     public bool Logout()
     {
+        _sw_last_action.Stop();
+        _sw_last_action.Reset();
+
         bool logged_in = IsLoggedIn;
 
         if (logged_in)
@@ -264,32 +330,138 @@ public sealed class APIConnection
             _driver.Navigate();
         }
 
+        lock (_keep_alive_mutex)
+        {
+            if (_keep_alive_task is { IsCompleted: false } task)
+                task.GetAwaiter().GetResult();
+
+            _keep_alive_task = null;
+        }
+
+        _current_phone = null;
+        _current_pin = null;
+
         return logged_in;
     }
 
     /// <summary>
+    /// Fetches all personal account holder data. Warning: this contains sensitive information.
+    /// </summary>
+    /// <exception cref="NotLoggedInException">Thrown if the user is not logged in.</exception>
+    /// <exception cref="ObjectDisposedException">Thrown if the API connection has already been closed or disposed.</exception>
+    /// <exception cref="TimeoutException"/>
+    /// <returns>The account holder's personal data.</returns>
+    public async Task<PersonalData> FetchPersonalData() => await LoggedInUserActionAsync<PersonalData>(async delegate
+    {
+        _driver.Url = _baseURL + URI__SETTINGS_PERSONAL;
+        _driver.Navigate();
+
+        string[]? personal_settings = (await WaitForElements(SEL__SETTINGS_PANEL_PERSONAL))?.Select(e => e.Text)?.ToArray();
+
+        _driver.Url = _baseURL + URI__SETTINGS_ACCOUNTS;
+        _driver.Navigate();
+
+        string[]? accounts_settings = (await WaitForElements(SEL__SETTINGS_PANEL_ACCOUNTS))?.Select(e => e.Text)?.ToArray();
+
+        _driver.Url = _baseURL + URI__SETTINGS_TAX;
+        _driver.Navigate();
+
+        string[]? tax_settings = (await WaitForElements(SEL__SETTINGS_PANEL_TAX))?.Select(e => e.Text)?.ToArray();
+
+        if (personal_settings is null || accounts_settings is null || tax_settings is null)
+            throw new TimeoutException();
+
+        return new(
+            Name: personal_settings[0],
+            PhoneNumber: personal_settings[1],
+            EMailAddress: personal_settings[2],
+            PostalAddress: personal_settings[3].Split(',').Select(l => l.Trim()).ToArray(),
+            BirthDate: DateTime.TryParse(personal_settings[4], out DateTime dt) ? dt : null,
+            PlaceOfBirth: personal_settings[5],
+            Citizenships: personal_settings[6].Split(',').Select(l => l.Trim()).ToArray(),
+            RefrenceAccountIBAN: accounts_settings[0],
+            CashAccountIBAN: accounts_settings[1],
+            SecuritiesAccountNumber: accounts_settings[2],
+            TaxID: tax_settings[0]
+        );
+    });
+
+    /// <summary>
+    /// Changes the current pin to the given new one.
+    /// </summary>
+    /// <param name="new_pin">The new pin.</param>
+    /// <exception cref="NotLoggedInException">Thrown if the user is not logged in.</exception>
+    /// <exception cref="ObjectDisposedException">Thrown if the API connection has already been closed or disposed.</exception>
+    public async Task ChangePin(ushort new_pin) => await LoggedInUserActionAsync(async delegate
+    {
+        if (!_current_pin.HasValue)
+            throw new NotLoggedInException();
+
+
+        //string? current_phone
+
+        throw new NotImplementedException();
+    });
+
+
+
+    private void LoggedInUserAction(Action action) => _ = LoggedInUserAction(delegate
+    {
+        action();
+
+        return false;
+    });
+
+    private T LoggedInUserAction<T>(Func<T> action)
+    {
+        if (IsDisposed)
+            throw new ObjectDisposedException(nameof(APIConnection));
+        else if (!IsLoggedIn)
+            throw new NotLoggedInException();
+
+        T result;
+
+        _sw_last_action.Stop();
+        _sw_last_action.Reset();
+
+        lock (_keep_alive_mutex)
+            result = action();
+
+        _sw_last_action.Start();
+
+        return result;
+    }
+
+    private async Task LoggedInUserActionAsync(Func<Task> action) => _ = await LoggedInUserActionAsync(async delegate
+    {
+        await action();
+
+        return false;
+    });
+
+    private async Task<T> LoggedInUserActionAsync<T>(Func<Task<T>> action) => await LoggedInUserAction(action);
+
+    private void InjectJQuery() => _driver.ExecuteScript(Resources.jquery);
+
+    /// <summary>
     /// Waits for the given elements to be selectable (by the web driver) and returns a non-empty collection if any element has been found before the timeout.
     /// </summary>
-    private IWebElement[]? WaitForElements(By selector, float ms_timeout = 10_000)
+    /// <exception cref="ObjectDisposedException">Thrown if the API connection has already been closed or disposed.</exception>
+    private async Task<IWebElement[]?> WaitForElements(By selector, float ms_timeout = 30_000)
     {
-        WebDriverWait wait = new(_driver, TimeSpan.FromMilliseconds(ms_timeout));
-        IWebElement[]? elements = null;
+        Stopwatch sw = new();
 
-        return wait.Until(_ =>
-        {
-            var result = _driver.FindElements(selector);
+        sw.Start();
 
-            if (result.Any())
-            {
-                elements = result.ToArray();
-
-                return true;
-            }
+        while (sw.ElapsedMilliseconds < ms_timeout)
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(APIConnection));
+            else if (_driver.FindElements(selector) is { } result && result.Any())
+                return result.ToArray();
             else
-                return false;
-        });
+                await Task.Delay(150);
 
-        return elements;
+        return null;
     }
 
     /// <summary>
@@ -305,10 +477,9 @@ public sealed class APIConnection
             {
                 Logout();
 
+                _driver.Quit();
                 _driver.Close();
                 _driver.Dispose();
-
-                // TODO
 
                 IsDisposed = true;
             }
@@ -350,3 +521,26 @@ public enum LoginStatus
     Failure_LoginTimeout,
 }
 
+public sealed record PersonalData(
+    string Name,
+    string PhoneNumber,
+    string EMailAddress,
+    string[] PostalAddress,
+    DateTime? BirthDate,
+    string PlaceOfBirth,
+    string[] Citizenships,
+    string RefrenceAccountIBAN,
+    string CashAccountIBAN,
+    string SecuritiesAccountNumber,
+    string TaxID
+    // TODO : exception order
+);
+
+public sealed class NotLoggedInException
+    : InvalidOperationException
+{
+    public NotLoggedInException()
+        : base("This operation could not be performed, as the user is not currently logged in.")
+    {
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+using TradeRepublic;
+
+
+using var connection = API.CreateAPIConnection();
+
+await connection.Login("+491776200214", 1488, () =>
+{
+    Thread.Sleep(500);
+    Console.Clear();
+    Console.WriteLine("please enter the sms pin:");
+    return ushort.Parse(Console.ReadLine() ?? "0");
+});
+Console.WriteLine(await connection.FetchPersonalData());
+Console.WriteLine("press ENTER to log out.");
+Console.ReadLine();
+connection.Logout();
+
+Debugger.Break();
